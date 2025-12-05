ae10ee5cc0115712b400af6fffcaaf7db189b8de
Nghianvworkhard <nghiacutepro2003.hhc@gmail.com>
2025-12-05 15:54:31 +0700

    Iter 1 set up model
    add category.js

--- infra-lab-backend/src/models/Device.js
+++ infra-lab-backend/src/models/Device.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const deviceSchema = new mongoose.Schema({
-    name: String,
-    category: String,
-    description: String,
-    image: String
+    name: { type: String, required: true },
+    category_id: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+    description: { type: String, default: "" },
+    image: { type: String, default: "" }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Device", deviceSchema);
