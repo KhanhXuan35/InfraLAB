2ff18b3cb9e3b79fcf58b6ded583c9d4da4af65a
Nghianvworkhard <nghiacutepro2003.hhc@gmail.com>
2025-12-07 17:11:28 +0700

    Iter 1 add auth route

---
+++ infra-lab-backend/src/routes/common/authRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { register, verifyEmail, login, refreshToken, googleLogin, logout } from "../../controllers/common/authController.js";
+import { checkAuthMiddleware } from "../../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.get("/verify-email/:token", verifyEmail); // Frontend gọi API này
+router.post("/login", login);
+router.post("/refresh-token", refreshToken);
+router.post("/google-login", googleLogin);
+
+router.post("/logout", checkAuthMiddleware, logout);
+
+export default router;
